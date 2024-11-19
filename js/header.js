ad7d2a58949a537e283a8e8feab930f98e241ced
HayaDassouki <dassoukihaya@gmail.com>
2024-11-19 20:57:28 +0200

    scroll feature for shop and contact

--- js/header.js
+++ js/header.js
@@ -1,8 +1,16 @@
-const navbar = document.querySelector('.nav-container')
+const navbar1 = document.querySelector('.scroll1')
 window.addEventListener('scroll', () => {
     if (window.scrollY > 550) {
-        navbar.classList.add('scrolled')
+        navbar1.classList.add('scrolled')
     } else {
-        navbar.classList.remove('scrolled')
+        navbar1.classList.remove('scrolled')
+    }
+})
+const navbar2 = document.querySelector('.scroll2')
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 350) {
+        navbar2.classList.add('scrolled')
+    } else {
+        navbar2.classList.remove('scrolled')
     }
 })
