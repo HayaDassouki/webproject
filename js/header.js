701cf221697fdfe9d833d4eee06fdd76a8a52883
HayaDassouki <dassoukihaya@gmail.com>
2024-11-26 18:47:44 +0200

    hamburger click--> X

--- js/header.js
+++ js/header.js
@@ -39,5 +39,8 @@ let navigation=document.getElementById('navbar')
 document.getElementById('hamburger').addEventListener('click',()=>{
     navigation.classList.toggle('show')    
 })
-
+let x=document.getElementById('hamburger')
+document.getElementById("hamburger").addEventListener("click", function () {
+  x.classList.toggle("hamclicked")
+})
 })
